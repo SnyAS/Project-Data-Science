08766507d4647ccfbdbe5e39f75ed3b507cd771e
Sny <semidaandreicha@gmail.com>
2018-12-05 11:43:36 +0100

    Update merging_yield.py

--- Tal_Merging/merging_yield.py
+++ Tal_Merging/merging_yield.py
@@ -99,15 +99,19 @@ compare each two corresponding columns and take the mean value of those column a
 drop all _x and _y columns after taking the mean of them and assign them to a new column, if column has no corresponding 
 column just keep it as it is
 '''
-
 # Write all unique dataframes to csv
-unique_inner_df.to_csv('C:/Users/Inholland/Desktop/dataset/yield_unique_inner_join.csv')
+unique_inner_df.to_csv('C:/Users/Inholland/Desktop/dataset/yield_unique_inner_join_test1.csv')
 
-print(len(unique_inner_df.columns))
+print(len(unique_inner_df.columns), unique_inner_df.columns)
 '''
 the rest of features which can't be compared
 'year', 'month', 'day', 'hr', 'min', 'sec', 'lon(degr)', 'lat(degr)', 'alt(m)',sats', 'x(m)', 'y(m)', '', 'usertare(%)', 'Hoogte', 'loadkg', '',
 '''
+
+f, ax = plt.subplots(figsize=(18, 18))
+#fmt='.1f' - numbers inside the correlation
+sns.heatmap(unique_inner_df.corr(), annot=True, linewidths=.5, fmt='.1f', ax=ax)
+plt.show()
 #returning ture ->can be merged
 print((unique_inner_df['workwidth(m)'].values == unique_inner_df['workwidth'].values).all()) #true
 print((unique_inner_df['qual_y'].values == unique_inner_df['qual_x'].values).all()) #true
@@ -133,7 +137,6 @@ print((unique_inner_df['totalarea'].values == unique_inner_df['totalarea(ha)'].v
 print((unique_inner_df['yieldton'].values == unique_inner_df['yield(ton/ha)'].values).all())
 print((unique_inner_df['loadbeltm'].values == unique_inner_df['loadbelt(m)'].values).all())
 
-
 # ['workwidth(m)', 'qual_y', 'tare(kg)', 'tarecorrectedtotalyield(ton)_y', 'totalyield(ton)_x', 'workwidth'] <- to be dropped
 keep_col= ['year', 'month', 'day', 'hr', 'min', 'sec', 'lon(degr)', 'lat(degr)',
        'alt(m)', 'qual_x', 'sats', 'x(m)', 'y(m)', 'speed(km/h)', 'load(kg)',
@@ -146,42 +149,39 @@ keep_col= ['year', 'month', 'day', 'hr', 'min', 'sec', 'lon(degr)', 'lat(degr)',
        'tarecorrectedyield(ton/ha)_y']
 
 unique_inner_df1 = unique_inner_df
-unique_inner_df1   = unique_inner_df1[keep_col]
+unique_inner_df1 = unique_inner_df1[keep_col]
 print(len(unique_inner_df1.columns), unique_inner_df.columns)
-unique_inner_df.to_csv('C:/Users/Inholland/Desktop/dataset/yield_unique_inner_join_no_duplicates1.csv')
+unique_inner_df1.to_csv('C:/Users/Inholland/Desktop/dataset/yield_unique_inner_join_no_duplicates_test2.csv')
 
-#renaming attributes
-unique_inner_df1 = unique_inner_df.rename(columns={'speedkmh': 'speed km/h', 'conv.facto': 'conv.factor', 'loadkg': 'load(kg)',
-'X': 'lon(degr)', 'Y': 'lat(degr)', 'Hoogte': 'alt(m)', 'tarekg': 'tare(kg)',
-'conv.facto': 'conv.factor', 'beltspdm': 'beltspd(m/s)', 'workwidth(': 'workwidth(m)',
-'yieldton': 'yield(ton/ha)', 'totalyield': 'totalyield(ton)',
-'point_weig': 'point weight (kg)', 'totalarea': 'totalarea(ha)',
-'worktimes': 'worktime(s)', 'loadweight': 'loadweight(ton)',
-'loadbeltm': 'loadbelt(m)',
-'tarecorrec': 'tarecorrectedyield(ton/ha)', 'tarecorre2': 'tarecorrectedtotalyield(ton)'})
+
 len(unique_inner_df1.columns)
 unique_inner_df1.columns
 
+cols = list(unique_inner_df1.columns)
 
+for col in cols:
+    unique_inner_df1[cols.columns.add_suffix('_average')] = unique_inner_df1[[col, col]].mean(axis=1)
 
 #taking the mean of _x and _y columns
-#unique_inner_df1.columns["conv.factor_average"] = unique_inner_df1.columns[:, "conv.factor_x", "conv.factor_y"].mean(axis=1) #axis=1 <-row wise 0 = column wise
+unique_inner_df1["conv.factor_average"] = unique_inner_df1[["conv.factor_x", "conv.factor_y"]].mean(axis=1) #axis=1 <-row wise 0 = column wise
+
 
 unique_inner_df1["conv.factor_x"].value_counts() # value: 30.1168 - count: 28206
 unique_inner_df1["conv.factor_y"].value_counts() # value: 30.117 - count: 28206
-unique_inner_df1["conv.factor_x"] = round(unique_inner_df["conv.factor_x"], 3)
-unique_inner_df1["conv.factor_x"].head() == unique_inner_df1["conv.factor_y"].head()
+#unique_inner_df1["conv.factor_x"] = round(unique_inner_df["conv.factor_x"], 3)
+unique_inner_df1["conv.factor_x"].head() == unique_inner_df1["conv.factor_y"].head() #returns false
 #unique_inner_df1["conv.factor_y"].head()
 #CAN DROP THE conv.factor because it contains the same value for each row so there's no point in keeping it
 #conv_factor_average = unique_inner_df.groupby(["conv.factor_x", "conv.factor_y"]).mean()
+#unique_inner_df1.columns["conv.factor_average"] = unique_inner_df1.columns["conv.factor_x"].mean(axis=1) #axis=1 <-row wise 0 = column wise
 
 
 unique_inner_df1["speed(km/h)"].value_counts() # mean: 6.230955  - count: 28206
-unique_inner_df1["speed km/h"].value_counts() # mean: 6.228926  - count: 28206
-unique_inner_df1["speed km/h"].describe()
+unique_inner_df1["speedkmh"].value_counts() # mean: 6.228926  - count: 28206
 unique_inner_df1["speed(km/h)"].describe()
+unique_inner_df1["speedkmh"].describe()
 #unique_inner_df1["_x"] = round(unique_inner_df["_x"], 3)
-(unique_inner_df1["speed(km/h)"].head(100) == unique_inner_df1["speed km/h"].head(100))#.all() <-when I do all they = False but when I look at the first 5 columns they returen 4 - true 1-false
+(unique_inner_df1["speed(km/h)"].head(100) == unique_inner_df1["speedkmh"].head(100))#.all() <-when I do all they = False but when I look at the first 5 columns they returen 4 - true 1-false
 
 
 unique_inner_df1["loadnr_y"].value_counts() # mean: 7.351485 count: 28206
@@ -197,14 +197,9 @@ unique_inner_df1["loadweight(ton)_x"].describe()
 unique_inner_df1["loadweight(ton)_y"].describe()
 (unique_inner_df1["loadweight(ton)_x"].head(10) == unique_inner_df1["loadweight(ton)_y"].head(10)) #returns true for majority so remove?
 
-unique_inner_df1["totalyield(ton)_x"].value_counts() # mean: 94.454683 count: 28206
-unique_inner_df1["totalyield(ton)_y"].value_counts() # mean: 94.422921  - count: 28206
-unique_inner_df1["totalyield(ton)_x"].describe()
-unique_inner_df1["totalyield(ton)_y"].describe()
 
-(unique_inner_df1["totalyield(ton)_y"].head(10) == unique_inner_df1["totalyield(ton)_x"].head(10)) #returns true for majority so remove?
-(unique_inner_df1["tarecorrectedtotalyield(ton)_x"].head() == unique_inner_df1["totalyield(ton)_x"].head()).all() #returns true for all (same)
-(unique_inner_df1["totalyield(ton)_y"].head() == unique_inner_df1["tarecorrectedtotalyield(ton)_y"].head()).all()#returns true for all(same)
+#(unique_inner_df1["tarecorrectedtotalyield(ton)_x"].head() == unique_inner_df1["totalyield(ton)_x"].head()).all() #returns true for all (same)
+#(unique_inner_df1["totalyield(ton)_y"].head() == unique_inner_df1["tarecorrectedtotalyield(ton)_y"].head()).all()#returns true for all(same)
 
 unique_inner_df1["tarecorrectedyield(ton/ha)_x"].describe()# mean: 47.118443 count: 28206
 unique_inner_df1["tarecorrectedyield(ton/ha)_y"].describe()# mean: 47.161643  - count: 28206
@@ -215,13 +210,18 @@ unique_inner_df1["point weight (kg)_y"].describe()# mean: 1.798181 count: 28206
 unique_inner_df1["point weight (kg)_x"].describe()# mean: 1.797609  - count: 28206
 (unique_inner_df1["point weight (kg)_x"].head(100) == unique_inner_df1["point weight (kg)_y"].head(100)) #returns true for majority so remove?
 
-unique_inner_df1["qual_y"].describe()# mean: 4 count: 28206
+#unique_inner_df1["qual_y"].describe()# mean: 4 count: 28206
 unique_inner_df1["qual_x"].describe()# mean: 4  - count: 28206
-(unique_inner_df1["qual_x"].head() == unique_inner_df1["qual_y"].head()).all() #returns true for all(same)
+#(unique_inner_df1["qual_x"].head() == unique_inner_df1["qual_y"].head()).all() #returns true for all(same)
 
-unique_inner_df1["workwidth"].describe()# mean: 2.223733 count: 28206
+#unique_inner_df1["workwidth"].describe()# mean: 2.223733 count: 28206
 unique_inner_df1["workwidth(m)"].describe()# mean: 2.223733  - count: 28206
-(unique_inner_df1["workwidth"].head() == unique_inner_df1["workwidth(m)"].head()).all() #returns true for all(same)
+#(unique_inner_df1["workwidth"].head() == unique_inner_df1["workwidth(m)"].head()).all() #returns true for all(same)
+
+f, ax = plt.subplots(figsize=(18, 18))
+#fmt='.1f' - numbers inside the correlation
+sns.heatmap(unique_inner_df1.corr(), annot=True, linewidths=.5, fmt='.1f', ax=ax)
+plt.show()
 
 
 '''
@@ -233,3 +233,13 @@ Remaining attributes to check mean
        'loadbelt(m)', 'usertare(%)', 'alt(m)', 'load(kg)', 'tare(kg)', , 'beltspd(m/s)', '',
        'yield(ton/ha)','totalarea(ha)', 'worktime(s)','loadbelt(m)']
 '''
+#renaming attributes
+"""unique_inner_df1 = unique_inner_df.rename(columns={'speedkmh': 'speed km/h', 'conv.factor_x': 'conv.factor', 'loadkg': 'load(kg)',
+'X': 'lon(degr)', 'Y': 'lat(degr)', 'Hoogte': 'alt(m)', 'tarekg': 'tare(kg)', 'qual_x': 'qual'
+'beltspdm': 'beltspd(m/s)', 'workwidth(': 'workwidth(m)',
+'yieldton': 'yield(ton/ha)', 'totalyield': 'totalyield(ton)',
+'point_weig': 'point weight (kg)', 'totalarea': 'totalarea(ha)',
+'worktimes': 'worktime(s)', 'loadweight': 'loadweight(ton)',
+'loadbeltm': 'loadbelt(m)',
+'tarecorrec': 'tarecorrectedyield(ton/ha)', 'tarecorre2': 'tarecorrectedtotalyield(ton)'})
+"""
